f0cb8c72200393a30575a3631cc6ac88eeb2c118
Jacob Yax <yaxjacob@css-050.i>
2023-10-26 22:17:00 -0400

    Updates

---
+++ lab8_line/line_detect/line_detect/detect.py
@@ -0,0 +1,101 @@
+'''
+    image_display.py 
+
+    This is an exmple ROS node for subscribing to image topics and displaying images
+    Can specify the image topic name as well as whether or not it is compressed
+      Usage:
+    ros2 run image_fun image_display <topic_name> 
+    ros2 run image_fun image_display <topic_name> --compressed
+
+    Use the optional --compressed argument if the image topic is type compressed
+
+    Daniel Morris, Nov 2020, 2022
+'''
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image, CompressedImage
+from geometry_msgs.msg import PointStamped
+import argparse
+import cv2 as cv
+from cv_bridge import CvBridge, CvBridgeError
+from scipy.special import expit
+import numpy as np
+
+class ImageDisplay(Node):
+    def __init__(self, topic_name, compressed):
+        super().__init__('image_display')        
+        self.compressed = compressed
+        self.publisher_ = self.create_publisher(PointStamped, 'line_point', 10)
+        self.title = f'{topic_name}, type: compressed' if compressed else f'{topic_name}, type: raw'
+        self.bridge = CvBridge()
+        self.get_logger().info(f'Subscribed to: {self.title}')
+        if self.compressed:
+            self.subscription = self.create_subscription(CompressedImage, topic_name, self.image_cb, 1)
+        else:
+            self.subscription = self.create_subscription(Image, topic_name, self.image_cb, 1)
+        self.subscription 
+        
+    def image_cb( self, msg ):
+        publish_msg = PointStamped()
+        if self.compressed:
+            img = self.bridge.compressed_imgmsg_to_cv2(msg,'bgr8')
+        else:
+            img = self.bridge.imgmsg_to_cv2(msg,'bgr8')
+
+        # crop last 5 rows to get the green part closest to robot
+        height, width, channels = img.shape
+        crop_height = 5
+        cropped_img = img[height - crop_height:,:]
+        
+        cvec = np.array([[ 0.16038173,  0.18115433, -0.37381468]]) 
+        intercept = 0.81807159
+
+        score = (cropped_img.astype(float) * cvec).sum(axis=2) + intercept
+        probt = expit(score)
+        threshold = 0.5
+
+        binary_target = (probt > threshold).astype(np.uint8) 
+        cc = cv.connectedComponentsWithStats(binary_target)
+
+        inds = np.argsort(cc[2][:,4]) # sort on number of pixels in each continguous region
+
+        centroid = np.zeros(2)
+        for i in inds[::-1]:
+            # If the average probability of target in region > 0.99
+            if binary_target[cc[1]==i].astype(float).mean() > 0.99:
+                # Then keep this region
+                centroid = cc[3][i,:]
+                break
+
+        # add back offset 
+        if np.any(centroid):
+            center = (int(centroid[0]), int(centroid[1] + height - crop_height))
+            publish_msg.point.x = centroid[0]
+            publish_msg.point.y = centroid[1]
+            img = cv.circle(img, center, radius=10, color=(0, 0, 255), thickness=-1)
+
+        publish_msg.header = msg.header
+
+        self.publisher_.publish(publish_msg)
+
+        cv.imshow(self.title, img )
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            raise SystemExit  # If user pressed "q"           
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    parser = argparse.ArgumentParser(description='Image type arguments')
+    parser.add_argument('topic_name',      default="/image",     type=str, help="Image topic to subscribe to")
+    parser.add_argument('--compressed',    action='store_true',            help='Set if compressed image message')
+    args, unknown = parser.parse_known_args()
+    if unknown: print('Unknown args:',unknown)
+
+    node = ImageDisplay( topic_name=args.topic_name, compressed=args.compressed)  
+    try:
+        rclpy.spin(node)       
+    except SystemExit:
+        node.destroy_node()
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        pass
---
+++ lab8_line/line_detect/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'line_detect'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yaxjacob',
+    maintainer_email='yaxjacob@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'labeler = line_detect.labeler:main',
+            'detect = line_detect.detect:main'
+        ],
+    },
+)
